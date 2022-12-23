4f57bc1b865a1ec149df59a0566545b64e1892c4
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2022-12-23 07:34:46 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
+#echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
 #echo 'src-git oui https://github.com/zhaojh329/oui.git' >>feeds.conf.default
